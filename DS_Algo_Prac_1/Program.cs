3f90c8b1979a26d190ab347aa391850b9c5672aa
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-12-06 06:56:19 +0530

    DS Demo 1

--- DS_Algo_Prac_1/Program.cs
+++ DS_Algo_Prac_1/Program.cs
@@ -10,6 +10,46 @@ namespace DS_Algo_Prac_1
     {
         static void Main(string[] args)
         {
+            int rad, height;
+            double area;
+            Console.WriteLine("Enter radius of Cylinder:");
+            rad = Convert.ToInt32(Console.ReadLine());
+
+            if (rad <= 0)
+            {
+                do
+                {
+                    Console.WriteLine("Radius cannot be 0 or negative. Please enter correct radius:");
+                    rad = Convert.ToInt32(Console.ReadLine());
+                } while (rad <= 0);
+            }
+
+            Console.WriteLine("Enter height of Cylinder:");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            if (height <= 0)
+            {
+                do
+                {
+                    Console.WriteLine("Height cannot be 0 or negative. Please enter correct height:");
+                    height = Convert.ToInt32(Console.ReadLine());
+                } while (height <= 0);
+            }
+
+            area = AreaOfCylinder(rad, height);
+
+            Console.WriteLine("Area of Cylinder is {0}", area);
+            Console.ReadLine();
+
+        }
+
+        public static double AreaOfCylinder(int radius, int height)
+        {
+            int radSq = radius * radius;
+            double pi = Math.PI;
+
+            double area = pi * radSq * height;
+            return area;
         }
     }
 }
