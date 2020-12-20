2db8942a7064d786ec874a7d6b092bb3f8493d7f
Mohammed Haseeb <Mohammed.Haseeb@aveva.com>
2020-12-20 17:33:24 +0530

    LeetCode Shuffle String with Bubble Sort

---
+++ DS_Algo_Prac_1/LeetCode_1528_ShuffleString_BubbleSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_Algo_Prac_1
+{
+    class LeetCode_1528_ShuffleString_BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string s = Console.ReadLine();
+
+            char[] sa = s.ToCharArray();
+
+            Console.WriteLine("Enter Indices:");
+            
+            char[] index = Console.ReadLine().ToCharArray();
+
+            int[] intarr = Array.ConvertAll(index, c => (int)Char.GetNumericValue(c));
+
+            bool swapped;
+            do
+            {
+                swapped = false;
+
+                for (int i = 0; i < intarr.Length-1; i++)
+                {
+                    if (intarr[i] > intarr[i + 1])
+                    {
+                        int temp = intarr[i + 1];
+                        char temp2 = sa[i + 1];
+
+                        intarr[i + 1] = intarr[i];
+                        sa[i + 1] = sa[i];
+
+                        intarr[i] = temp;
+                        sa[i] = temp2;
+
+                        swapped = true;
+                    }
+                }
+            } while (swapped == true);
+
+            string snew = "";
+            for(int i=0; i<sa.Length; i++)
+            {
+                snew = snew + sa[i];
+            }
+
+            Console.WriteLine("String before: {0}", s);
+            Console.WriteLine("String after: {0}", snew);
+
+            Console.ReadLine();
+        }
+    }
+}
